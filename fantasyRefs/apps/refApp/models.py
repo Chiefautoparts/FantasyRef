c94830a5fcfe33d9d1238fe3d02892e6a1d7acbe
chiefautoparts <chiefautoparts@outllook.com>
2017-07-20 16:02:03 -0700

    ghost css

--- fantasyRefs/apps/refApp/models.py
+++ fantasyRefs/apps/refApp/models.py
@@ -15,7 +15,7 @@ class UserManager(models.Manager):
         if not postData['username'] or len(postData['username']) < 3:
             status['valid'] = False
             status['errors'].append('Username isnt good enough to be on this site. Pick a new one or leave')
-        if not postData['email'] or not re.match(r'(^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9]+\.[a-zA-Z0-9-.]+$)', postData['email']):
+        if not postData['email'] or not re.match(r'(^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$)', postData['email']):
              status['valid'] = False
              status['errors'].append('INVALID EMAIL!!!!')
         if not postData['password'] or len(postData['password']) < 6:
@@ -24,43 +24,44 @@ class UserManager(models.Manager):
         if postData['confirm_password'] != postData['password']:
             status['valid'] = False
             status['errors'].append('Your Passwords Don\'t match Dumbass.')
-          
+        if status['valid'] is False:
+            return status 
+        
         user = User.objects.filter(username=postData['username'])
 
+        if user:
+            status['valid'] = False
+            status['errors'].append('you dun Fucked Up. Come on it doesn\'t take rocket appliances do do this')
+
         if status['valid']:
-            try:
-                user = User.objects.create(
+               user = User.objects.create(
                     name=postData['name'],
                     username=postData['username'],
                     email=postData['email'],
-                    password=(bcrypt.hashpw(postData['password'].encode(), bcrypt.gensalt())))
-                print user.password
-                user.save()
-                status['user'] = user
-            except IntegrityError as e:
-                status['valid'] = False
-                # if 'UNIQUE constraint' in e.message:
-                #     status['errors'].append('that email already exists, duuude parallel universe')
-                # else:
-                #     status['errors'].append(e.message)
-            return status
+                    password=(bcrypt.hashpw(postData['password'].encode(), bcrypt.gensalt()))
+                    )
+               user.save()
+               status['user'] = user
+        return status
 
     def loginValidation(self, postData):
         status = {'valid':True, 'errors': [], 'user':None}
+        user = User.objects.filter(username=postData['username'])
         try:
-            user = User.objects.filter(username=postData['username'])
-            if user.password == (bcrypt.hashpw(postData['password'].encode(), user.password.encode())):
-                pass
-            else:
-                raise Exception()
-        except Exception as e:
+            user[0]
+        except IndexError:
             status['valid'] = False
-            status['errors'].append("WRONG!!!!!")
+            status['errors'].append('No account is found so you are wrong. DO IT AGAIN!!')
 
-        if status['valid']:
-            status['user'] = user
-        return status
-
+        if user[0]:
+            if user[0].password !=bcrypt.hashpw(postData['password'].encode(), user[0].password.encode()):
+                status['status'] = False
+                status['errors'].append('WRONG!!!')
+            else:
+                status['user'] = user[0].id   
+        else:
+            status['status'] = False
+        return logged
 
 class User(models.Model):
     name = models.CharField(max_length=255)
